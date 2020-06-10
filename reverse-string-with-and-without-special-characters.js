e6fefb29b40796cbebed8c842dcacd469cfb3379
Akash Kumar Singh <akash.singh22396@gmail.com>
2020-06-10 15:40:35 +0530

    Create reverse-string-with-and-without-special-characters.js

---
+++ reverse-string-with-and-without-special-characters.js
@@ -0,0 +1,31 @@
+
+ const reverseWord=(data)=> data.split('').reverse().join('')
+
+const reverseWordWithoutSpecialChar = (data) => {
+  let strData = data.toLowerCase().split("");
+  let numMt = "0123456789".split("");
+  let strMt = "abcdefghijklmnopqrstuvwxyz".split("");
+  // console.log(strData,numMt,strMt);
+  let newArr = [];
+  let specArr = [];
+  strData.forEach((dt) => {
+    // console.log(dt,strMt.indexOf(dt)> -1,numMt.indexOf(dt)> -1)
+    if (strMt.indexOf(dt) > -1 || numMt.indexOf(dt) > -1) {
+      newArr.push(dt);
+    } else {
+      specArr.push(dt);
+    }
+  });
+  let reverseAr = newArr.reverse().join("");
+  specArr.forEach((dt) => {
+    let leftH = reverseAr.substr(0, data.indexOf(dt));
+    let rightH = reverseAr.substr(data.indexOf(dt));
+    reverseAr = leftH + dt + rightH;
+  });
+  //    console.log(reverseAr);
+  return reverseAr;
+};
+
+let str = "-1nig#t9@";
+console.log(reverseWord(str));
+console.log(reverseWordWithoutSpecialChar(str));
